019b40af7741ce9716068e275cb4e62b08fdf1b8
José Iván Chapoñán Casiano <ivan@navike21.com>
2019-03-02 14:39:00 -0500

    last changes
    Ultiimos cambios pe siono

--- js/ready.js
+++ js/ready.js
@@ -1,25 +1,32 @@
-loadDoc = (name, email, phone) => {
-  let xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      // document.getElementById("demo").innerHTML = this.responseText;
-      console.log(this.responseText)
-    }
-  };
-  xhttp.open("POST", "https://precisionhealthcenters.com/sendmail/", true);
-  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhttp.send("name=" + name + "&email=" + email + "&phone=" + phone);
+loadDoc = () => {
+  let fullname = document.getElementById('fullname');
+  let email = document.getElementById('email');
+  let phone = document.getElementById('phone');
+  let message = document.getElementById("message");
+  let dataComplete = false;
+
+  if(fullname.value !== "" && email.value !== "" && phone.value !== ""){
+  
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        message.classList.add("fadeIn")
+        setTimeout(() => {
+          message.classList.remove("fadeIn");
+          fullname.value = "";
+          email.value = "";
+          phone.value = "";
+        }, 2500);
+      }
+    };
+    xhttp.open("POST", "../sendmail/", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("name=" + fullname.value + "&email=" + email.value + "&phone=" + phone.value);
+  }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   // Handler when the DOM is fully loaded
   let formData = document.getElementById('formData');
-  let fullname = document.getElementById('fullname');
-  let email = document.getElementById('email');
-  let phone = document.getElementById('phone');
-  let dataComplete = false;
-
-  if(fullname.val !== "" && email.val !== "" && phone.val !== ""){
-    formData.addEventListener("submit", loadDoc, true);
-  }
+  formData.addEventListener("submit", loadDoc, true);
 });
