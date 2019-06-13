64ddd67a27126f24bc7db8b0149b5f215ec4e068
navike21 <ivan@navike21.com>
2019-06-12 23:28:45 -0500

    initial files wordpress

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="<?php bloginfo('language'); ?>">
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title><?php bloginfo('name'); ?></title>
+  <?php wp_head(); ?>
+  <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/favicon.ico" />
+</head>
+<body>
